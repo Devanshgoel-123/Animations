44a41e6836f78d78c15e4db2eb37d9437b718b5f
Devanshgoel-123 <117383704+Devanshgoel-123@users.noreply.github.com>
2023-01-18 01:35:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// 
+// $(".box").slideUp().slideDown();
+// $(".dbox").slideUp().slideDown();
+// document.addEventListener("keypress",addShadow);
+// document.addEventListener("click",addShadow);
+//
+//
+// function addShadow(){
+//   $(".dbox").addClass("shadow");
+//   $(".box").addClass("shadow");
+//
+//   setTimeout(function(){
+//     $(".dbox").removeClass("shadow");
+//     $(".box").removeClass("shadow");
+//   },200);
+// }
